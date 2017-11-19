5460c625d20a0148f78d63a188b375a84881d1b1
Lukas Lazarek <LukasALazarek@gmail.com>
2017-11-19 15:19:55 -0500

    Implemented module death and replacement handlers

--- src/octopOS_driver.cpp
+++ src/octopOS_driver.cpp
@@ -116,3 +116,81 @@ bool accessible(FilePath file) {
     struct stat buffer;
     return (stat (name.c_str(), &buffer) == 0);
 }
+
+void kill_module(std::string path, ModuleInfo *modules) {
+    Module &module = (*modules)[path];
+    module.killed = true;
+    kill(module.pid, SIGTERM);
+}
+
+bool module_needs_downgrade(ModuleInfo module) {
+    // TODO(llazarek): Is this a sufficient heuristic?
+    return (time(0) - module.launch_time) < RUNTIME_CUTOFF_DOWNGRADE_S;
+}
+
+void reboot_module(std::string path, ModuleInfo *modules) {
+    Module &module = (*modules)[path];
+    if (module.killed) {
+	// Death was intentional, just reboot
+	module.killed = false;
+	module.pid = launch(path, module.msgkey);
+	module.launch_time = time(0);
+    } else {
+	// Something else happened
+	if (module_needs_downgrade(module)) {
+	    downgrade(path);
+	} else {
+	    module.pid = launch(path, module.msgkey);
+	    module.launch_time = time(0);
+	}
+    }
+}
+
+Optional<std::string> find_module_with(pid_t pid, ModuleInfo &modules) {
+    auto moduleIt = std::find_if(modules.begin(), modules.end(),
+				 [](const Module &module) {module.pid == pid});
+    if (moduleIt == modules.end()) {
+	return Optional::None();
+    } else {
+	return Optional::Just(moduleIt -> first);
+    }
+}
+
+class ChildHandler {
+    static void sigchld_handler(int sig)
+    {
+	pid_t p;
+	int status;
+	int x = 0;
+	pthread_t tmp;
+
+	while ((pid = waitpid(-1, &status, WNOHANG)) != -1) {
+	    // handle all dead children in new threads
+	    pthread_create(&tmp, NULL, handle_dead_module, &x);
+	}
+    }
+
+    static void handle_dead_module(pid_t pid) {
+	Optional<std::string> found = find_module_with(pid, modules);
+	if (found.isEmpty()) {
+	    std::cerr << "Notification of unregistered module death. "
+		      << "Something has probably gone horribly wrong."
+		      << std::endl;
+	} else {
+	    reboot_module(found.get(), &modules);
+	}
+    }
+
+    static void register_child_death_handler(ModuleInfo *_modules) {
+	modules = *_modules;
+	struct sigaction sa;
+
+	memset(&sa, 0, sizeof(sa));
+	sa.sa_handler = sigchld_handler;
+
+	sigaction(SIGCHLD, &sa, NULL);
+    }
+
+private:
+    static ModuleInfo &modules;
+};
