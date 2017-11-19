05b8147dd0c09ff1822af920b694958ac21e11fd
Lukas Lazarek <LukasALazarek@gmail.com>
2017-11-19 15:20:43 -0500

    Implemented main function for driver

--- src/Main.cpp
+++ src/Main.cpp
@@ -5,29 +5,46 @@
 
 // Copyright 2016 UMass Lowell Command and Data Handling Team
 
-void setup();
-void loop();
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <pthread.h>
+
+#include <octopOS/octopos.h>
+#include "../include/octopOS_driver.hpp"
 
 int main(int argc, char const *argv[]) {
-  setup();
-  loop();
-  return 0;
-}
+    octopOS::octopOS octopos = octopOS::getInstance();
+    MemKey current_key = MSGKEY;
 
-/*!
- * Initializes core components of program, and begins a networking thread with
- * ground station. 
- */
-void setup() {
-  // TODO(Team Req): Give functionality
-}
+    Optional<json> maybe_config = load(CONFIG_PATH);
+    if (maybe_config.isEmpty()) {
+	std::cerr << "Critical Error: Unable to read config at "
+		  << CONFIG_PATH
+		  << ". Exiting..." << std::endl;
+	return 1;
+    }
+    json config = maybe_config.get();
 
-/*!
- * Takes commands from ground station, queues them up, and runs them by 
- * command priority. 
- */
-void loop() {
-  while (true) {
-    // TODO(Team Req): Give functionality
-  }
+    LaunchInfo launched = launch_modules_in(config["modules_enabled"],
+					    current_key);
+    current_key = launched.second;
+    ModuleInfo modules = launched.first;
+
+    if (!launch_listeners()) {
+	std::cerr << "Critical Error: Unable to spawn listener threads."
+		  << "Exiting..." << std::endl;
+	return -1;
+    }
+
+    ChildHandler::register_child_death_handler(&modules);
+
+    reboot_module = modules[REBOOT_TOPIC];
+    subscriber<std::string> reboot_sub(REBOOT_TOPIC, reboot_module.msgkey);
+
+    while (LISTEN_FOR_MODULE_REBOOTS) {
+	std::string module_path = reboot_sub.getData();
+	kill_module(module_path, *modules);
+    }
 }
